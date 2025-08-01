b60349adab6b19db039e9aff9581591ae49236fa
josuke-higashikata <bowles.joshp@gmail.com>
2025-07-31 23:05:32 -0400

    added event manifest

--- VintageCCI/VintageCCI.cs
+++ VintageCCI/VintageCCI.cs
@@ -15,6 +15,9 @@ namespace VintageCCI
 {
     public class VintageCCI : ModSystem
     {
+        const string VINTAGECCI_FILE_PREFIX = "VintageCCI_event";
+        const string MANIFEST_FILENAME = VINTAGECCI_FILE_PREFIX + "_manifest.json";
+
         private TwitchUsernamePrompt m_prompt;
         private CCIClient m_client;
         //private TwitchInterface m_interface;
@@ -50,6 +53,76 @@ namespace VintageCCI
             api.Input.SetHotKeyHandler("twitchusername", ToggleTwitchUsernamePrompt);
         }
 
+        // Keeping this here for later
+        //private void MakeEvent()
+        //{
+        //    ev = new Event();
+        //    ev.Trigger = new Trigger(TriggerType.CHAT_MESSAGE, "!die");
+        //    ev.Conditions.Add(Condition.Create<bool>("the_scrongl", false, CompareType.EQUAL, ConditionJoin.NONE));
+        //    ev.Bindings.Add(new Binding(BindingType.COMMAND, "/kill"));
+        //    api.StoreModConfig(ev.ToJson(), filename);          
+        //}
+
+        private bool TryLoadEvents(ICoreClientAPI api)
+        {
+            try
+            {
+                var manifest = api.LoadModConfig(MANIFEST_FILENAME);
+                JObject m;
+                if (manifest != null)
+                {
+                    if (manifest.Token.Type != JTokenType.Object)
+                    {
+                        Mod.Logger.Notification("Manifest is not the proper format");
+                        return false;
+                    }
+                    m = (JObject)manifest.Token;
+
+                    if (!m.ContainsKey("manifest") || m["manifest"].Type != JTokenType.Array)
+                    {
+                        Mod.Logger.Notification("Manifest is not the proper format");
+                        return false;
+                    }
+
+                    foreach (var event_name in m["manifest"])
+                    {
+                        if (event_name.Type != JTokenType.String)
+                        {
+                            Mod.Logger.Notification("Manifest is not the proper format");
+                            return false;
+                        }
+
+                        Event e;
+                        if (!TryLoadEvent(api, event_name.ToString(), out e))
+                        {
+                            Mod.Logger.Notification($"Failed to load file \"{event_name}\", continuing.");
+                            continue;
+                        }
+
+                        if (!CCIClient.RegisterEvent(e))
+                        {
+                            Mod.Logger.Notification($"Failed to parse event \"{event_name}\", continuing.");
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    Mod.Logger.Notification($"Manifest file \"{MANIFEST_FILENAME}\" not found! Making a new one.");
+                    m = new JObject(){
+                        new JProperty("manifest", new JArray())
+                    };
+                    api.StoreModConfig(m, MANIFEST_FILENAME);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Mod.Logger.Notification($"Failed to load due to an exception: {e.ToString()}");
+                return false;
+            }
+        }
+
         private bool TryLoadEvent(ICoreClientAPI api, string filename, out Event ev)
         {
             try
@@ -57,23 +130,21 @@ namespace VintageCCI
                 var res = api.LoadModConfig(filename);
                 if (res == null)
                 {
-                    ev = new Event();
-                    ev.Trigger = new Trigger(TriggerType.CHAT_MESSAGE, "!die");
-                    ev.Conditions.Add(Condition.Create<bool>("the_scrongl", false, CompareType.EQUAL, ConditionJoin.NONE));
-                    ev.Bindings.Add(new Binding(BindingType.COMMAND, "/kill"));
-                    
-                    api.StoreModConfig(ev.ToJson(), filename);
+                    ev = null;
+                    return false;
                 }
                 else 
                 {
                     JToken result = res.Token;
-                    Event.FromJson((JObject)result, out ev);
+                    if (!Event.FromJson((JObject)result, out ev))
+                        return false;
                 }
 
                 return true;
             }
             catch (Exception ex)
             {
+                Mod.Logger.Notification($"Failed to load due to an exception: {ex.ToString()}");
                 ev = null;
                 return false;
             }
@@ -109,8 +180,7 @@ namespace VintageCCI
             RegisterVariables();
 
             Event ev;
-            if (TryLoadEvent(api, "event_scrongl.json", out ev))
-                CCIClient.RegisterEvent(ev);
+            TryLoadEvents(api);
         }
 
         public override void StartServerSide(ICoreServerAPI api)
